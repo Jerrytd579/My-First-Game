7d2d34888a9ab57259c4cf96b3c7cfa4acee8b3b
Jerry Cheng <jerrytd579@gmail.com>
2019-06-28 20:47:13 -0400

    first commit

---
+++ enemies/stalfos.gd
@@ -0,0 +1,20 @@
+extends "res://engine/entity.gd"
+
+
+var movetimer_length = 15
+var movetimer = 0
+
+func _ready():
+	SPEED = 40
+	$anim.play('default')
+	movedir = dir.rand()
+
+func _physics_process(delta):
+	movement_loop()
+	damage_loop()
+	
+	if movetimer > 0:
+		movetimer -= 1
+	if movetimer == 0 || is_on_wall():
+		movedir = dir.rand()
+		movetimer = movetimer_length
---
+++ engine/entity.gd
@@ -0,0 +1,71 @@
+extends KinematicBody2D
+
+const MAXHEALTH = 2
+var TYPE = 'ENEMY'
+var SPEED = 0
+var DAMAGE = 1
+
+var movedir = Vector2(0,0)
+var knockdir = Vector2(0,0)
+var spritedir = 'down'
+
+var hitstun = 0
+var health = MAXHEALTH
+
+var texture_default = null
+var texture_hurt = null
+
+func _ready():
+	texture_default = $Sprite.texture
+	texture_hurt = load($Sprite.texture.get_path().replace('.png','_hurt.png'))
+
+func movement_loop():
+	var motion
+	if hitstun == 0:
+		motion = movedir.normalized() * SPEED
+	else:
+		motion = knockdir.normalized() * 125
+	move_and_slide(motion, Vector2(0,0))
+
+func spritedir_loop():
+	match movedir:
+		Vector2(-1,0):
+			spritedir = 'left'
+		Vector2(1,0):
+			spritedir = 'right'
+		Vector2(0,-1):
+			spritedir = 'up'
+		Vector2(0,1):
+			spritedir = 'down'
+
+func anim_switch(animation):
+	var newanim = str(animation,spritedir)
+	if $anim.current_animation != newanim:
+		$anim.play(newanim)
+
+func damage_loop():
+	if hitstun > 0:
+		hitstun -= 1
+		$Sprite.texture = texture_hurt
+	else:
+		$Sprite.texture = texture_default
+		if TYPE == 'ENEMY' && health <= 0:
+			var death_animation = preload("res://enemies/enemy_death.tscn").instance()
+			get_parent().add_child(death_animation)
+			death_animation.global_transform = global_transform
+			queue_free()
+	for area in $hitbox.get_overlapping_areas():
+		var body = area.get_parent()
+		
+		if hitstun == 0 and body.get('DAMAGE') != null and body.get('TYPE') != TYPE:
+			health -= body.get('DAMAGE')
+			hitstun = 10
+			knockdir = global_transform.origin - body.global_transform.origin
+
+func use_item(item):
+	var newitem = item.instance()
+	newitem.add_to_group(str(newitem.get_name(), self))
+	add_child(newitem)
+	
+	if get_tree().get_nodes_in_group(str(newitem.get_name(),self)).size() > newitem.maxamount:
+			newitem.queue_free()
